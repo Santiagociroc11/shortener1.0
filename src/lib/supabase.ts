b7a9f4c7898bcfdfdb36736d2d42b5589f58cda9
Santiagociroc11 <santiagociiro12@gmail.com>
2025-02-16 21:35:41 -0500

    vifinal

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,9 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log("SUP1", supabaseUrl)
-console.log("SUP2", supabaseAnonKey)
-
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Please click the "Connect to Supabase" button in the top right to set up your project');
 }
